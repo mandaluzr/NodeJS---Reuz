161c9331517302521f1060a53a732da30b5af4e9
Jorge <cv.jreyes@gmail.com>
2021-02-02 16:53:34 +0100

    fix to method create on productsController

--- src/resources/products/products.controller.js
+++ src/resources/products/products.controller.js
@@ -13,18 +13,27 @@ const getAll = async (req, res) => {
     return res.status(404).end();
   };
   
-/*   const create = (req, res) => {
-    const creationDate = new Date();
-    const newproduct = {...req.body,created_at:creationDate,updated_at:creationDate};
-    const productsUpdated = productModel.create(newproduct);
-    return res.status(201).json(productsUpdated);
-  }; */
-
   const create = (req, res) => {
+    const payload = req.body;
+      const creationDate = new Date();
+      if (Array.isArray(req.body)){
+      const newproducts = payload.map(function(product) {
+          return {... product, created_at:creationDate, updated_at:creationDate};
+      });
+      const productsUpdated = productModel.create(newproducts);
+      return res.status(201).json(productsUpdated);
+      }else{
+        const newproduct = {... payload, created_at:creationDate, updated_at:creationDate};
+        const productsUpdated = productModel.create(newproduct);
+        return res.status(201).json(productsUpdated);
+      }
+    };
+
+/*   const create = (req, res) => {
     const newproduct = req.body;
     const productsUpdated = productModel.create(newproduct);
     return res.status(201).json(productsUpdated);
-  };
+  }; */
   
   const update = (req, res) => {
     const creationDate = new Date();
