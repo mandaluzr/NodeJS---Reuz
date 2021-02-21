e9d6002a62f066f55070c43a5e11f1bbeae60695
raulstvz <estevezterrados@gmail.com>
2021-02-21 20:18:51 +0100

    minor changes in auth controller to expose user id

--- src/resources/auth/auth.controller.js
+++ src/resources/auth/auth.controller.js
@@ -4,10 +4,17 @@ const jwt = require('jsonwebtoken');
 const login = async(req, res) => {
     const { email, password } = req.body;
     const user = await userModel.getByEmail(email);
+    const userInfo = {
+      id: user._id,
+      email: user.email,
+    }
     if (user.password === password)
     {
       const token = jwt.sign({email: email, role: 'admin'}, process.env.TOKEN_SECRET);
-      res.json(token);
+      res.json({
+        token: token,
+        user: userInfo
+      });
     } 
     else {
       res.status(401).send("Username or password incorrect");
